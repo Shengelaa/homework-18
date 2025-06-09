ef2c038df3c6e7b7f7998fd08c573864ab215fd6
Shengela <168460082+Shengelaa@users.noreply.github.com>
2025-06-09 05:09:06 +0400

    rrr

--- frontend/config/sockets.ts
+++ frontend/config/sockets.ts
@@ -1,6 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000");
-// const socket = io("https://homework-18-egxs.onrender.com");
+const socket = io("https://homework-18-egxs.onrender.com");
 
 export default socket;
